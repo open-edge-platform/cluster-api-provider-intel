f810f67586052792d2101745d222749ced460e24
Lazar, Madalina <madalina.lazar@intel.com>
2025-04-02 11:13:57 +0000

    fix: use cluster name when creating a workload in inventory
    Signed-off-by: Lazar, Madalina <madalina.lazar@intel.com>

--- internal/controller/intelcluster_controller_test.go
+++ internal/controller/intelcluster_controller_test.go
@@ -91,7 +91,7 @@ var _ = Describe("IntelCluster Controller", func() {
 				Once()
 
 			Expect(kerrors.IsNotFound(k8sClient.Get(ctx, clusterConnectionFilter, &ccgv1.ClusterConnect{}))).To(BeTrue())
-			clusterConnection = getClusterConnectionManifest(intelCluster, cluster)
+			clusterConnection = getClusterConnectionManifest(cluster)
 			Expect(k8sClient.Create(ctx, clusterConnection)).To(Succeed())
 		})
 
@@ -218,7 +218,7 @@ var _ = Describe("IntelCluster Controller", func() {
 				Return(inventory.CreateWorkloadOutput{WorkloadId: "", Err: errors.New("test")})
 
 			Expect(kerrors.IsNotFound(k8sClient.Get(ctx, clusterConnectionFilter, &ccgv1.ClusterConnect{}))).To(BeTrue())
-			clusterConnection = getClusterConnectionManifest(intelCluster, cluster)
+			clusterConnection = getClusterConnectionManifest(cluster)
 			Expect(k8sClient.Create(ctx, clusterConnection)).To(Succeed())
 		})
 
