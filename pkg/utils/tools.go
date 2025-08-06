77d83b085512dc7fc2b31a1d25560cf4a05a5b3f
Moon, Hyunsun <hyunsun.moon@intel.com>
2025-08-05 17:50:56 -0700

    chore: remove unnecessary function

--- pkg/utils/tools.go
+++ pkg/utils/tools.go
@@ -6,9 +6,6 @@ package utils
 import (
 	"bytes"
 	"context"
-	"crypto/tls"
-	"encoding/base64"
-	"io"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -96,42 +93,6 @@ func RetrieveFQDN(urls string) (string, error) {
 		return "", errors.ValidationErr(err, "error parsing URL")
 	}
 	return up.Host + up.Path, nil
-}
-
-// Get CA from URL
-// example:
-// Get CA from https://registry.intel.com/hello
-// return: encodedCA, error(nil)
-func GetCA(caPath string) (string, error) {
-	if IsUrl(caPath) {
-		log.Info().Msgf("need to put CA to Vault secret path")
-
-		transport := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, //nolint: gosec // need to allow insecure for svc-to-svc communication
-		}
-		client := &http.Client{Transport: transport}
-		resp, err := client.Get(caPath) //nolint: noctx // FIXME: This is not the right way per linter issue. Check more
-		// here https://github.com/sonatard/noctx to fix it. More effort and change in this PR and hence skipping this.
-		if err != nil {
-			log.Err(err).Msgf("get CA from CA path error: %v", err)
-			return "", err
-		}
-
-		defer func() {
-			_ = resp.Body.Close()
-		}()
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Err(err).Msgf("get CA from CA path error: %v", err)
-			return "", err
-		}
-		ca := base64.StdEncoding.EncodeToString(body)
-		return ca, nil
-	} else {
-		// TODO process secret path type caPath
-		return "", nil
-	}
 }
 
 // Terminate Sidecar
